bd996bdc1677a5d117aeae4637f65ab8ae5461e0
LuisMagit <luismapanama@gmail.com>
2025-01-20 09:57:42 +0100

    ..

--- lib/constants/p_my_stuff.dart
+++ lib/constants/p_my_stuff.dart
@@ -3,5 +3,5 @@ class PMyStuff {
   static const github = 'https://github.com/LuisMaGit';
   static const linkedin = 'https://www.linkedin.com/in/luisma93';
   static const mail = 'mailto:lgfernandez2023@gmail.com';
-  static const cv = 'https://drive.google.com/file/d/1ziDyrw2eZEqlSyUQN8y9TCFKFnwkgZVy/view?usp=sharing';
+  static const cv = 'https://drive.google.com/file/d/1hWFMuvJXRRlk5O6ziRuiawC5ksbI1IUB/view?usp=drive_link';
 }
